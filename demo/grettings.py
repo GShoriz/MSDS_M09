948520c2d242627589645d1e276d5d0d1f79d03f
George Shoriz <georgeshoriz29@gmail.com>
2023-10-24 22:59:24 -0400

    Done

---
+++ demo/grettings.py
@@ -0,0 +1,3 @@
+def grettings ():
+    """gretting a person"""
+    print("Hello stranger, I'm a student at UVA studying Data Science.")
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name= "Demo",
+    version= '0.1',
+    description= 'A gretting message'
+    url= 'https://github.com/GShoriz/MSDS_M09/tree/main/demo'
+    author='George Shoriz'
+    author_email='georgeshoriz22@gmail.com'
+    license='MIT'
+    packages=['demo']
+)
