596f47e53668f61f4287d733a132058013c399be
George Shoriz <georgeshoriz29@gmail.com>
2023-10-24 23:07:09 -0400

    Done

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name= "Demo",
     version= '0.1',
-    description= 'A gretting message'
-    url= 'https://github.com/GShoriz/MSDS_M09/tree/main/demo'
-    author='George Shoriz'
-    author_email='georgeshoriz22@gmail.com'
-    license='MIT'
+    description= 'A gretting message',
+    url= 'https://github.com/GShoriz/MSDS_M09/tree/main/demo',
+    author='George Shoriz',
+    author_email='georgeshoriz22@gmail.com',
+    license='MIT',
     packages=['demo']
 )
